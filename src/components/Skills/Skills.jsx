de558715a52eb3272fa21ef4136435b565a2c0e2
avinash.webintern@gmail.com <avinash.webintern@gmail.com>
2024-05-17 17:01:37 +0530

    Added new skills in skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -16,7 +16,7 @@ import Canva from '../../assets/images/skills/canva.png';
 import Git from '../../assets/images/skills/git.png';
 import GitHub from '../../assets/images/skills/github.png';
 import NodeJS from '../../assets/images/skills/nodejs.png';
-// import MongoDB from '../../assets/images/skills/mongodb.png';
+import MongoDB from '../../assets/images/skills/mongo.png';
 // Import other images as needed
 
 
@@ -38,7 +38,7 @@ export const Skills = () => {
         { src: Canva, alt: 'canva' },
         { src: Tailwind, alt: 'tailwind' },
         { src: NodeJS, alt: 'nodejs' },
-        // { src: MongoDB, alt: 'mongo' },
+        { src: MongoDB, alt: 'mongo' },
     ];      
     // Carousel responsive configuration
     const responsive = {
